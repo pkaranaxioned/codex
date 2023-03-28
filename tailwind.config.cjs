7b87abe0f4bb827bf6b4f1abdda33952c6e43035
iamkaranpatel <krn.rj.pte@gmail.com>
2023-03-28 22:26:16 +0530

    tailwing plugin and new utility class added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin');
+
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -8,10 +10,32 @@ module.exports = {
 		extend: {
 			colors: {
 				'ax-violet': '#201547',
-				'dark-grey': '#151010',
+				'dark-grey': '#0D0D0D',
 				'dark-grey-2': '#262525',
+				'light-grey': '#ffffff99',
+				'red': '#ff6363',
+				'light-red': '#fd828226'
 			},
+			spacing: {
+        '60': '60vh',
+        '13': '13vh',
+      }
 		},
 	},
-	plugins: [],
+	plugins: [
+		plugin(function({ addUtilities }) {
+      addUtilities({
+				/* Hide scrollbar for Chrome, Safari and Opera */
+				'.no-scrollbar::-webkit-scrollbar': {
+						'display': 'none',
+				},
+
+				/* Hide scrollbar for IE, Edge and Firefox */
+				'.no-scrollbar': {
+						'-ms-overflow-style': 'none', /* IE and Edge */
+						'scrollbar-width': 'none',  /* Firefox */
+				}
+      })
+    })
+	],
 }
