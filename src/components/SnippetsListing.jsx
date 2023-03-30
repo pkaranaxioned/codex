3eb46404fd34dc4d21a29c0c8cb7a56e948b3201
ahsanaaxioned <ahsana@axioned.com>
2023-03-30 17:23:43 +0530

    Tags Added On Hover And Scroll Updated

--- src/components/SnippetsListing.jsx
+++ src/components/SnippetsListing.jsx
@@ -4,8 +4,8 @@ import { IconBrandGooglePhotos } from '@tabler/icons-react';
 export const SnippetsListing = () => {
   return (
     <>
-      <div className='basis-1/5 text-light-grey bg-zinc-950'>
-        <div className='flex  flex-col'>
+      <div className='basis-1/5 text-light-grey bg-zinc-950 border-r-[1px] border-[#1d1f1e]'>
+        <div className='flex  flex-col py-5'>
           <h2 className='flex justify-center py-4 '>
             <IconBrandGooglePhotos color="white" size={24} stroke={1} />
             <span className='px-2'>Google Photos</span>
@@ -21,11 +21,11 @@ export const SnippetsListing = () => {
             <button className='text-white font-semibold text-lg'>Clip</button>
             <p className='text-sm py-3'>Clips that you save using the Google Clips app will appear in Google Photos or your phone's gallery.</p>
           </li>
-          <li className='border-b-[1px] cursor-pointer py-2 md:hover:font-bold md:hover:text-purple-400 md:hover:border-b-purple-400 transition-all'>
-            <button className='text-white font-semibold text-lg flex '>Sign In</button>
-            <p className='text-sm py-3'>Go to Google Photos. If you aren't signed in to your Google Account, click Go to Google Photos and sign in.</p>
-            <div className='mt-3'>
-              <h2 className='text-white font-semibold text-lg'>Tags</h2>
+          <li className='border-b-[1px] group cursor-pointer text-white py-2 md:hover:font-bold md:hover:text-purple-400 md:hover:border-b-purple-400 transition-all duration-[0.3s]'>
+            <button className='font-semibold text-lg flex '>Sign In</button>
+            <p className='text-sm py-3 text-light-grey group-hover:text-purple-400 transition-all duration-[0.3s]'>Go to Google Photos. If you aren't signed in to your Google Account, click Go to Google Photos and sign in.</p>
+            <div className='mt-3 h-0 overflow-hidden  group-hover:h-full   transition-all duration-[0.3s]'>
+              <h2 className='font-semibold text-lg '>Tags</h2>
               <ul className='py-4 flex flex-wrap [&>:not(:last-child)]:mr-2'>
                 <li><a href="#FIXME" title="#react" className='block md:hover:bg-ax-violet md:hover:font-bold md:hover:font-bold md:hover:text-purple-400  rounded-full text-light-grey text-sm bg-light-pink py-1 px-3 mb-2'>#react</a></li>
                 <li><a href="#FIXME" title="#redux" className='block md:hover:bg-ax-violet md:hover:font-bold md:hover:font-bold md:hover:text-purple-400 rounded-full text-light-grey text-sm bg-light-pink py-1 px-3 mb-2'>#redux</a></li>
