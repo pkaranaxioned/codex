3a2922c53148a9bbfb5d6750ac77e848dfe1a921
iamkaranpatel <krn.rj.pte@gmail.com>
2023-04-03 13:24:32 +0530

    snippet listing style added after 1024px and logo added

--- src/components/SnippetsListing.jsx
+++ src/components/SnippetsListing.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import { IconBrandGooglePhotos } from '@tabler/icons-react';
+import { useStore } from '@nanostores/react';
+import { loadCatListing } from '../store/snippetCatListing';
 
 export const SnippetsListing = () => {
+  const isCatListingVisible = useStore(loadCatListing);
   return (
     <>
-      <div className='basis-1/5 text-light-grey bg-zinc-950 border-r-[1px] border-[#1d1f1e]'>
+      <div className='basis-2/6 lg:basis-1/5 text-light-grey bg-zinc-950 border-r-[1px] border-[#1d1f1e]'>
         <div className='flex  flex-col py-5'>
-          <h2 className='flex justify-center py-4 '>
-            <IconBrandGooglePhotos color="white" size={24} stroke={1} />
-            <span className='px-2'>Google Photos</span>
-          </h2>
+          <div className="flex flex-col items-start lg:flex-row m-auto w-4/5 lg:w-auto justify-between lg:justify-center">
+            <button className='mr-2 w-10 h-10 lg:hidden block' onClick={() => loadCatListing.set(!isCatListingVisible)}>
+              <img src="assets/Codex-logo.svg" alt="Codex" />
+            </button>
+            <h2 className='flex justify-center py-4 '>
+              <IconBrandGooglePhotos color="white" size={24} stroke={1} />
+              <span className='px-2'>Google Photos</span>
+            </h2>
+          </div>
           <form action="#FIXME" name='Search' method='post' className='w-4/5  mx-[auto]'>
             <div className='w-full'>
               <input type="text" name="text" placeholder='Search' className='w-full outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-purple-500 focus:border-purple-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-purple-500 dark:focus:border-purple-500' />
