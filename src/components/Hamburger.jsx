8e03deecd014e6bb4f7e06cc568f7a799c76a7d3
iamkaranpatel <krn.rj.pte@gmail.com>
2023-04-07 21:25:50 +0530

    class changed to classname

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -7,7 +7,7 @@ export const Hamburger = () => {
 const $slideSubCatListing = useStore(slideSubCatListing);
 
   return (
-    <button class='mb-2 md:hidden inline-block cursor-pointer hamburger' onClick={() => slideSubCatListing.set(!$slideSubCatListing) }>
+    <button className='mb-2 md:hidden inline-block cursor-pointer hamburger' onClick={() => slideSubCatListing.set(!$slideSubCatListing) }>
     <IconMenu2 color="white" size={36} stroke={1} />
   </button>
   )
