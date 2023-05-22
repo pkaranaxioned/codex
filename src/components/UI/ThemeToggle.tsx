a2fc16ba631551998d28677936719bccd358b0da
saifaliaxioned <saifalis@axioned.com>
2023-05-22 11:43:49 +0530

    theme toggle structure updated

--- src/components/UI/ThemeToggle.tsx
+++ src/components/UI/ThemeToggle.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Icons } from '../icon';
-type themeProp = {
-    isMobile: boolean
-}
 
-export const ToggleTheme: React.FC<themeProp> = ({isMobile=false}) => {
+export const ToggleTheme: React.FC = () => {
     const [theme, setTheme] = useState<string>('dark');
     const [icon, setIcon] = useState<JSX.Element | null>(
         <Icons.system className="text-white" size={24} stroke={1} />
@@ -14,9 +11,9 @@ export const ToggleTheme: React.FC<themeProp> = ({isMobile=false}) => {
     // Set icon based on theme
     useEffect(() => {
         setIcon(
-            theme === 'dark' ? <Icons.darkTheme className="text-white" size={24} stroke={1} /> : theme === 'light'
-                ? <Icons.lightTheme className="text-white" size={24} stroke={1} />
-                : <Icons.system className="text-white" size={24} stroke={1} />
+            theme === 'dark' ? <Icons.darkTheme className="text-pink" size={24} stroke={1} /> : theme === 'light'
+                ? <Icons.lightTheme className="text-pink" size={24} stroke={1} />
+                : <Icons.system className="text-pink" size={24} stroke={1} />
         );
     }, [theme]);
 
@@ -44,39 +41,40 @@ export const ToggleTheme: React.FC<themeProp> = ({isMobile=false}) => {
     }, [theme]);
 
     return (
-        <div className={`${isMobile? 'block' : 'hidden'} absolute top-6 right-[2.5%]`}>
+        <div className={`absolute top-6 right-[2.5%]`}>
             <button onClick={toggleThemeDropdown} className="flex mr-1 items-center text-white font-bold">
                 <span>{icon}</span>
                 <span className="text-[14px] ml-[3px]">Theme</span>
             </button>
             <ul
                 className={`${dropDownState ? 'block' : 'hidden'
-                    } w-36 bg-slate-800 overflow-hidden rounded text-left absolute z-10 top-[30px] right-[2.3%] list-none pl-0`}
+            } [&>:not(:last-child)]:border-b-[1px] [&>:not(:last-child)]:border-light-grey w-36 bg-slate-800 overflow-hidden rounded text-left absolute z-10 top-[30px] right-[2.3%] list-none pl-0`}
+        
             >
                 <li
-                    className={`cursor-pointer m-0 p-1 hover:bg-slate-500 ${theme === 'light' ? 'bg-slate-500' : ''}`}
+                    className={`cursor-pointer m-0 p-1 hover:bg-slate-500 ${theme === 'light' ? 'bg-slate-500 text-white font-bold' : ''}`}
                     onClick={() => handleThemeSelection('light')}
                 >
                     <span>
-                        <Icons.lightTheme className="text-white px-1 inline-block" size={28} stroke={1} />
+                        <Icons.lightTheme className={`${theme === 'light' ? 'text-pink font-bold' : 'text-white'} px-1 inline-block`} size={28} stroke={1} />
                     </span>
                     <span className="text-[15px]">Light</span>
                 </li>
                 <li
-                    className={`cursor-pointer m-0 p-1 hover:bg-slate-500 ${theme === 'dark' ? 'bg-slate-500' : ''}`}
+                    className={`cursor-pointer m-0 p-1 hover:bg-slate-500 ${theme === 'dark' ? 'bg-slate-500 text-white font-bold' : ''}`}
                     onClick={() => handleThemeSelection('dark')}
                 >
                     <span>
-                        <Icons.darkTheme className="text-white px-1 inline-block" size={28} stroke={1} />
+                        <Icons.darkTheme className={`${theme === 'dark' ? 'text-pink font-bold' : 'text-white'} px-1 inline-block`} size={28} stroke={1} />
                     </span>
                     <span className="text-[15px]">Dark</span>
                 </li>
                 <li
-                    className={`cursor-pointer m-0 p-1 hover:bg-slate-500 ${theme === 'system' ? 'bg-slate-500' : ''}`}
+                    className={`cursor-pointer m-0 p-1 hover:bg-slate-500 ${theme === 'system' ? 'bg-slate-500 text-white font-bold' : ''}`}
                     onClick={() => handleThemeSelection('system')}
                 >
                     <span>
-                        <Icons.system className="text-white px-1 inline-block" size={28} stroke={1} />
+                        <Icons.system className={`${theme === 'system' ? 'text-pink font-bold' : 'text-white'} px-1 inline-block`} size={28} stroke={1} />
                     </span>
                     <span className="text-[15px]">OS default</span>
                 </li>
