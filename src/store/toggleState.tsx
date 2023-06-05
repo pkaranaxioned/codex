517406efefe413ec9e1c67a9a6d15639845f1c81
ahsanaaxioned <ahsana@axioned.com>
2023-06-05 17:37:35 +0530

    animation updated on menu bar

--- src/store/toggleState.tsx
+++ src/store/toggleState.tsx
@@ -1,3 +1,3 @@
 import { atom } from "nanostores";
-export const isCategory = atom<boolean>(false);
-export const isSnippets = atom<boolean>(false);
+export const isCategory = atom<boolean | null>(null);
+export const isSnippets = atom<boolean | null>(null);
